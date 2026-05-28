bcbc4d5d21de70772a7e2ceb3a829c6a49c5e427
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-05-28 18:13:53 +0900

    jqを修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # entrypoint.sh
 if [ -n "$DB_SECRET_JSON" ]; then
-  export DB_USERNAME=$(echo "$DB_SECRET_JSON" | jq -r '.username')
-  export DB_PASSWORD=$(echo "$DB_SECRET_JSON" | jq -r '.password')
+  export DB_USERNAME=$(echo "$DB_SECRET_JSON" | python3 -c "..." -r '.username')
+  export DB_PASSWORD=$(echo "$DB_SECRET_JSON" | python3 -c "..." -r '.password')
 fi
 exec java -jar /app/app.jar "$@"
