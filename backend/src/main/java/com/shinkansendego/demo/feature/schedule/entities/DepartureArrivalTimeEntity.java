1d76686df92813d5214dea1786b7d1bb9862a9ee
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-06-04 11:53:58 +0900

    Time型をLocalTime型に変更

--- backend/src/main/java/com/shinkansendego/demo/feature/schedule/entities/DepartureArrivalTimeEntity.java
+++ backend/src/main/java/com/shinkansendego/demo/feature/schedule/entities/DepartureArrivalTimeEntity.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.sql.Time;
+
+import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
@@ -13,7 +14,7 @@ import java.sql.Time;
 public class DepartureArrivalTimeEntity {
     private String time_cd;
     private String schedule_cd;
-    private Time departure_time;
-    private Time arrival_time;
+    private LocalTime departure_time;
+    private LocalTime arrival_time;
     private String section_cd;
 }
