7a28a4481ca51f879f397344384c095b2f92afb0
TerasakiKaho <kterasaki@jeisryokai.onmicrosoft.com>
2026-06-04 13:37:18 +0900

    S1-PBI①-3_データベース設計 (#24)
    * データINSERTのsql追加
    
    * M_DepartureArrivalTimeのINSERT文
    
    * 空行の追加
    
    * レビュー指摘修正
    
    ---------
    
    Co-authored-by: terasaki.kaho <terasaki.kaho@wing.sysrdc.com>

---
+++ backend/src/main/resources/sql/M_Station.sql
@@ -0,0 +1,8 @@
+-- 新幹線駅テーブル
+USE postgres;
+
+CREATE TABLE M_Station(
+    station_cd VARCHAR(5) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+INSERT INTO M_Station (station_cd,name) VALUES ('THK01','東京'),('THK02','上野');
