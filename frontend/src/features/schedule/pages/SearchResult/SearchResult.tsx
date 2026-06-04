8e2386641444cac3538ff419e5901aebabec9f35
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-04 13:54:42 +0900

    inputにonChangeを追加

--- frontend/src/features/schedule/pages/SearchResult/SearchResult.tsx
+++ frontend/src/features/schedule/pages/SearchResult/SearchResult.tsx
@@ -30,12 +30,24 @@ export function SearchResult() {
           </h1>
           <div className="flex justify-between bg-primary-light rounded-2xl p-8 gap-4">
             <div className="flex flex-col w-1/2 items-start">
-              <label>出発日</label>
-              <input type="date"></input>
+              <label htmlFor="date">出発日</label>
+              <input
+                id="date"
+                type="date"
+                className="w-full bg-white"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              ></input>
             </div>
             <div className="flex flex-col w-1/2 items-start">
-              <label>出発時刻</label>
-              <input type="time"></input>
+              <label htmlFor="time">出発時刻</label>
+              <input
+                id="time"
+                type="time"
+                className="w-full bg-white"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+              ></input>
             </div>
           </div>
           {/* <Suspense fallback={<ScheduleListSkeleton />}> */}
