92c8187970796411e5fee4a9dfb7ab58f9cbacff
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-05 16:53:20 +0900

    ダイヤが0件の時の処理を追加

--- frontend/src/features/schedule/components/ScheduleList/ScheduleList.tsx
+++ frontend/src/features/schedule/components/ScheduleList/ScheduleList.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 import _ReactPaginate from "react-paginate";
 const ReactPaginate = (_ReactPaginate as any).default || _ReactPaginate;
 import "tailwindcss";
@@ -24,30 +25,52 @@ export function ScheduleList({ searchRequestDto }: ScheduleListProps) {
         <div className="self-end">
           {schedules.length}件の列車が見つかりました
         </div>
-        {schedules.slice(offset, offset + perPage).map((schedule, index) => {
-          return (
-            <ScheduleItem
-              key={index}
-              schedule={schedule}
-              departure_station_name={searchRequestDto.departure_station_name}
-              arrival_station_name={searchRequestDto.arrival_station_name}
+        {schedules.length ? (
+          <>
+            {schedules
+              .slice(offset, offset + perPage)
+              .map((schedule, index) => {
+                return (
+                  <ScheduleItem
+                    key={index}
+                    schedule={schedule}
+                    departure_station_name={
+                      searchRequestDto.departure_station_name
+                    }
+                    arrival_station_name={searchRequestDto.arrival_station_name}
+                  />
+                );
+              })}
+            <ReactPaginate
+              pageCount={Math.ceil(schedules.length / perPage)}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={2}
+              onPageChange={handlePageChange}
+              previousLabel={"前へ"}
+              nextLabel={"次へ"}
+              containerClassName="flex justify-center space-x-2"
+              pageLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
+              activeLinkClassName="bg-primary text-white cursor-not-allowed"
+              previousLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
+              nextLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
+              disabledLinkClassName="bg-gray-300 cursor-not-allowed"
             />
-          );
-        })}
-        <ReactPaginate
-          pageCount={Math.ceil(schedules.length / perPage)}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={2}
-          onPageChange={handlePageChange}
-          previousLabel={"前へ"}
-          nextLabel={"次へ"}
-          containerClassName="flex justify-center space-x-2"
-          pageLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
-          activeLinkClassName="bg-primary text-white cursor-not-allowed"
-          previousLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
-          nextLinkClassName="border-2 border-primary-light rounded-lg px-4 py-2 cursor-pointer"
-          disabledLinkClassName="bg-gray-300 cursor-not-allowed"
-        />
+          </>
+        ) : (
+          <div className="flex justify-center items-center h-96 border-2 rounded-2xl border-primary-light p-8">
+            <div className="w-full max-w-xs flex flex-col items-center gap-4">
+              <div className="w-fit h-fit p-3 bg-primary-light rounded-full text-5xl">
+                <AiOutlineExclamationCircle />
+              </div>
+              <div className="text-xl font-bold">
+                指定日時の列車はありません
+              </div>
+              <div className="text-base">
+                お選びいただいた日時以降の列車が見つかりませんでした。条件を変更するか翌日の列車を検索してください。
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
