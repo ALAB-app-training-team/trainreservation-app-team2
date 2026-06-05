d6a87399e613e4d62b07907330c43f422f1fba69
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-05 17:45:13 +0900

    フロント動作確認用データ追加

--- frontend/src/features/schedule/hooks/useSchedules.ts
+++ frontend/src/features/schedule/hooks/useSchedules.ts
@@ -19,7 +19,66 @@ export default function useSchedules() {
     // });
     // const [suchedules, setSchedules] = useState<SearchResponseDto[]>(data);
     const [schedules, setSchedules] = useState<SearchResponseDto[]>([
-
+        {
+            train_type_name: "はやぶさ1号",
+            departure_time: "06:32",
+            arrival_time: "06:39",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
+        {
+            train_type_name: "やまびこ41号",
+            departure_time: "06:40",
+            arrival_time: "06:47",
+        },
     ]);
 
     return { schedules }
