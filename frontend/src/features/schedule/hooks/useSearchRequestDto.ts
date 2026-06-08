2c7f28a6081f5f3ef9e4daf0f44f81bad08c8a96
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-08 14:52:00 +0900

    検索用データの修正

--- frontend/src/features/schedule/hooks/useSearchRequestDto.ts
+++ frontend/src/features/schedule/hooks/useSearchRequestDto.ts
@@ -3,8 +3,8 @@ import type { SearchRequestDto } from "../types/SearchRequestDto";
 
 export function useSearchRequestDto() {
     // TODO: 検索画面ができたら、useLocationで取り出すようにする
+    const [date, setDate] = useState<string>("2026-06-01");
     const [time, setTime] = useState<string>("12:00:00");
-    const [date, setDate] = useState<string>("2026-02-05");
     const [departureStation, setDepartureStation] = useState<string>("東京");
     const [arrivalStation, setArrivalStation] = useState<string>("上野");
     // const [time, setTime] = useState<string>("");
@@ -12,8 +12,8 @@ export function useSearchRequestDto() {
     // const [departureStation, setDepartureStation] = useState<string>("");
     // const [arrivalStation, setArrivalStation] = useState<string>("");
     const searchRequestDto: SearchRequestDto = {
-        time,
         date,
+        time,
         departure_station_name: departureStation,
         arrival_station_name: arrivalStation,
     };
