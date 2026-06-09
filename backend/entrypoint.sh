7e080aa91a45c57ea66a69b4c15d1a69ef004f3d
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-06-09 17:28:41 +0900

    パース確認

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# entrypoint.sh
 
 echo "DB_SECRET_JSON exist? -> $DB_SECRET_JSON"
+echo "Parsed:"
+echo "$DB_SECRET_JSON" | jq .
+echo "Password"
+echo "$DB_SECRET_JSON" | jq -r '.password'
 if [ -n "$DB_SECRET_JSON" ]; then
   export DB_USERNAME=$(echo "$DB_SECRET_JSON" | jq -r '.username')
   export DB_PASSWORD=$(echo "$DB_SECRET_JSON" | jq -r '.password')
