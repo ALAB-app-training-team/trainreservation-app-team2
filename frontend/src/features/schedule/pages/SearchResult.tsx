5a0d8551e4e3f3fc6cbd0f42bb9a86d5ae7b0bb1
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-10 17:01:28 +0900

    ホームボタンを追加

--- frontend/src/features/schedule/pages/SearchResult.tsx
+++ frontend/src/features/schedule/pages/SearchResult.tsx
@@ -1,5 +1,6 @@
-import { Suspense, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Suspense } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { GoHome } from "react-icons/go";
 import "tailwindcss";
 import { DepartureDateAndTimePicker } from "../components/DepartureDateAndTimePicker";
 import { ScheduleList } from "../components/ScheduleList/ScheduleList";
@@ -8,6 +9,7 @@ import { useSearchRequestDto } from "../hooks/useSearchRequestDto";
 
 export function SearchResult() {
   const location = useLocation();
+  const navigate = useNavigate();
   const condition = location.state;
   const { setTime, setDate, searchRequestDto } = useSearchRequestDto({
     condition,
@@ -17,10 +19,21 @@ export function SearchResult() {
     <>
       <div className="flex justify-center">
         <div className="w-full max-w-5xl flex flex-col gap-4 m-8">
-          <h1 className="text-left !text-3xl !m-0">
-            {searchRequestDto.departure_station_name}→
-            {searchRequestDto.arrival_station_name}
-          </h1>
+          <div className="flex justify-between">
+            <h1 className="text-left !text-3xl !m-0">
+              {searchRequestDto.departure_station_name}→
+              {searchRequestDto.arrival_station_name}
+            </h1>
+            <button
+              onClick={() => {
+                navigate("/searchSchedule");
+              }}
+              className="flex items-center gap-2 px-4 border-2 rounded-xl border-primary-light cursor-pointer"
+            >
+              <GoHome />
+              <div>ホーム</div>
+            </button>
+          </div>
           <div className="flex flex-col md:flex-row justify-between bg-primary-light rounded-2xl p-8 gap-4">
             <DepartureDateAndTimePicker
               id="date"
