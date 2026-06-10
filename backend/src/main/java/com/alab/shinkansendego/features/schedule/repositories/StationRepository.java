8cc52eb680b08b42e9bdff6f088aeafb2904b733
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-06-10 18:49:24 +0900

    新幹線駅全件検索APIを作成

--- backend/src/main/java/com/alab/shinkansendego/features/schedule/repositories/StationRepository.java
+++ backend/src/main/java/com/alab/shinkansendego/features/schedule/repositories/StationRepository.java
@@ -1,9 +1,14 @@
 package com.alab.shinkansendego.features.schedule.repositories;
 
+import com.alab.shinkansendego.features.schedule.dtos.StationResponseDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface StationRepository {
     String findStationCdByName(@Param("name") String name);
+
+    List<StationResponseDto> findAllStationCdAndName();
 }
---
+++ backend/src/main/java/com/alab/shinkansendego/features/schedule/servicies/SearchService.java
@@ -0,0 +1,23 @@
+package com.alab.shinkansendego.features.schedule.servicies;
+
+import com.alab.shinkansendego.features.schedule.dtos.StationResponseDto;
+import com.alab.shinkansendego.features.schedule.repositories.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchService {
+    private final StationRepository stationRepository;
+
+    @Autowired
+    public SearchService(
+            StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public List<StationResponseDto> getAllStationList(){
+        return stationRepository.findAllStationCdAndName();
+    }
+}
