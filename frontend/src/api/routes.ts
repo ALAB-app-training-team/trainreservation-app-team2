7d0eb0e0d16cecd62905b542029ecbbf297eb3a5
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-10 11:16:34 +0900

    新幹線駅全件取得・selectlistの実装

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -1,4 +1,8 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const BASE_URL = `${apiBaseUrl}/api`;
 const SCHEDULES = `${BASE_URL}/shinkansen-schedule`;
-export const ENDPOINTS = { SCHEDULES_SEARCH: () => SCHEDULES };
+const STATIONS = `${BASE_URL}/shinkansen-station`;
+export const ENDPOINTS = {
+  SCHEDULES_SEARCH: () => SCHEDULES,
+  STATIONS: () => STATIONS,
+};
--- frontend/src/features/schedule/components/SearchScheduleBody/SearchScheduleBody.tsx
+++ frontend/src/features/schedule/components/SearchScheduleBody/SearchScheduleBody.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { ENDPOINTS } from "../../../../api/routes";
 import { DepartureDateAndTimePicker } from "../DepartureDateAndTimePicker";
 import { useSearchRequestDto } from "../../hooks/useSearchRequestDto";
+import type { Station } from "../../types/Station";
 
 export function SearchScheduleBody() {
   const {
@@ -10,7 +15,18 @@ export function SearchScheduleBody() {
     searchRequestDto,
   } = useSearchRequestDto();
 
-const 
+  /*   const { data } = useSuspenseQuery({
+    queryKey: ["station"],
+    queryFn: async () => {
+      const response = await axios.get<Station[]>(ENDPOINTS.STATIONS());
+      return response.data;
+    },
+  });
+  const [stations, setStations] = useState<Station[]>(data); */
+  const [stations, setStations] = useState<Station[]>([
+    { station_cd: "tokyo", name: "東京" },
+    { station_cd: "ueno", name: "上野" },
+  ]);
 
   return (
     <>
@@ -21,15 +37,22 @@ const
             <div className="flex flex-col md:flex-row justify-between gap-4">
               <div className="flex flex-col gap-2 w-full items-start">
                 <label htmlFor="date">乗車駅</label>
-                <input
-                  id="departureStation"
-                  type=
-                  className="w-full bg-white p-2 rounded-xl outline-none border-2 border-transparent focus:border-primary-light"
-                  value={value}
-                  onChange={(e) => setValue(e.target.value)}
-                />{/* input,selectlist, datalistを調べて乗車駅・降車駅を実装する */}
                 <select>
-                    
+                  {stations.map((station) => {
+                    return <option value={station.name}>{station.name}</option>;
+                  })}
+                </select>
+              </div>
+              <div className="flex flex-col gap-2 w-full items-start">
+                <label htmlFor="date">降車駅</label>
+                <select>
+                  {stations.map((station, index) => {
+                    return (
+                      <option key={index} value={station.name}>
+                        {station.name}
+                      </option>
+                    );
+                  })}
                 </select>
               </div>
             </div>
@@ -49,7 +72,8 @@ const
                 setValue={setTime}
               />
             </div>
-            <button>列車を検索</button>{/* SearchResultにNavigateする（検索条件をstateで渡す） */}
+            <button>列車を検索</button>
+            {/* SearchResultにNavigateする（検索条件をstateで渡す） */}
           </div>
         </div>
       </div>
