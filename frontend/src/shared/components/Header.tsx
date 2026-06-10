ae6af624927016d6fc4ab83da14ec170c4609b7b
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-10 17:09:22 +0900

    検索結果画面でもボタンの背景色をつけるように修正

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -1,8 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { BsTrainFreightFrontFill } from "react-icons/bs";
 
 export function Header() {
-  const buttons = [{ label: "新幹線を探す", to: "/searchSchedule" }];
+  const location = useLocation();
+  const buttons = [
+    {
+      label: "新幹線を探す",
+      to: "/searchSchedule",
+      relatedPath: ["/searchResult"],
+    },
+  ];
 
   return (
     <div className="relative h-16 flex items-center justify-start gap-6 px-8 border-primary-light border-b-2">
@@ -19,7 +26,13 @@ export function Header() {
             key={index}
             to={button.to}
             className={({ isActive }) =>
-              `rounded-xl px-4 py-3 text-sm font-bold ${isActive ? "bg-primary text-white" : "cursor-pointer"}`
+              `rounded-xl px-4 py-3 text-sm font-bold ${
+                isActive
+                  ? "bg-primary text-white"
+                  : button.relatedPath.includes(location.pathname)
+                    ? "cursor-pointer bg-primary-light"
+                    : "cursor-pointer"
+              }`
             }
           >
             {button.label}
