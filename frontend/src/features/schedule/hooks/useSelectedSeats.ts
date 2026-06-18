4f0854bd5c22becc0954305f727f932349b90820
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-18 11:00:04 +0900

    不要なimportを削除

--- frontend/src/features/schedule/hooks/useSelectedSeats.ts
+++ frontend/src/features/schedule/hooks/useSelectedSeats.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function useSelectedSeats() {
   const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
