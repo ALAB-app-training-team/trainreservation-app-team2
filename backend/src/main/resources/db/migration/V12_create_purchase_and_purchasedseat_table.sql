ac3cf7d035fd92169e700dfd0a5a7256e2043044
yasuda.risa <ryasuda@jeis-bi.net>
2026-06-23 16:54:50 +0900

    code_tokenを作成

--- backend/src/main/resources/db/migration/V12_create_purchase_and_purchasedseat_table.sql
+++ backend/src/main/resources/db/migration/V12_create_purchase_and_purchasedseat_table.sql
@@ -8,13 +8,14 @@ CREATE TABLE T_Purchase
     ride_date            DATE       NOT NULL,
     schedule_cd          VARCHAR(6) NOT NULL REFERENCES M_Schedule (schedule_cd) ON DELETE CASCADE,
     departure_station_cd VARCHAR(5) NOT NULL REFERENCES M_Station (station_cd) ON DELETE CASCADE,
-    arrival_station_cd   VARCHAR(5) NOT NULL REFERENCES M_Station (station_cd) ON DELETE CASCADE,
+    arrival_station_cd   VARCHAR(5) NOT NULL REFERENCES M_Station (station_cd) ON DELETE CASCADE
 );
 
 CREATE TABLE T_PurchasedSeat
 (
     id           UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    purchase_id  UUID       NOT NULL REFERENCES T_Purchase (id) ON DELETE CASCADE,
-    train_car_cd VARCHAR(7) NOT NULL REFERENCES M_TrainCar (train_car_cd) ON DELETE CASCADE,
-    seat_cd      VARCHAR(9) NOT NULL REFERENCES M_Seat (seat_cd) ON DELETE CASCADE,
+    purchase_id  UUID                           NOT NULL REFERENCES T_Purchase (id) ON DELETE CASCADE,
+    train_car_cd VARCHAR(7)                     NOT NULL REFERENCES M_TrainCar (train_car_cd) ON DELETE CASCADE,
+    seat_cd      VARCHAR(9)                     NOT NULL REFERENCES M_Seat (seat_cd) ON DELETE CASCADE,
+    code_token   UUID DEFAULT gen_random_uuid() NOT NULL UNIQUE
 );
