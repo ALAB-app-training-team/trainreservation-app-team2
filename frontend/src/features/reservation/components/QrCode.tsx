6587c5a28d0bff678a47400ff9142aebe7f3846e
hashimoto.tatsuya <hashimoto.tatsuya@wing.sysrdc.com>
2026-06-25 11:17:04 +0900

    create QRcode

---
+++ frontend/src/features/reservation/components/QrCode.tsx
@@ -0,0 +1,53 @@
+import { QRCodeSVG } from "qrcode.react";
+
+type QrCodeProps = {
+  train_car_type_name: string;
+  train_car_number: string;
+  seat_number: string;
+  seat_column: string;
+  code_token: string;
+  code_token_text: string;
+};
+
+export const QrCode = ({
+  train_car_type_name,
+  train_car_number,
+  seat_number,
+  seat_column,
+  code_token,
+  code_token_text,
+}: QrCodeProps) => {
+  return (
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-2xl p-6 shadow-sm text center">
+      <h2 className="text-2xl font-bold text-gray-800 mb-1">
+        {train_car_type_name}
+      </h2>
+      <p className="text-sm text-gray-500 mb-6">{`${train_car_number} 号`}</p>
+      <p className="text-sm text-gray-500 mb-6">
+        {seat_number}
+        {seat_column}
+      </p>
+
+      <div className="flex justify-center mb-4">
+        <QRCodeSVG
+          value={code_token}
+          size={180}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"H"}
+          marginSize={4}
+          imageSettings={{
+            src: "../../../../../public/BsTrainFreightFrontFill.svg",
+            x: undefined,
+            y: undefined,
+            height: 20,
+            width: 20,
+            excavate: true,
+          }}
+        />
+      </div>
+
+      <p className="text-sm text-gray-400">QRコード: {code_token_text}</p>
+    </div>
+  );
+};
--- frontend/src/features/schedule/pages/SearchSchedule.tsx
+++ frontend/src/features/schedule/pages/SearchSchedule.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 import { SearchScheduleBody } from "../components/SearchScheduleBody/SearchScheduleBody";
 import { SearchScheduleBodySkeleton } from "../components/SearchScheduleBody/SearchScheduleBodySkeleton";
+import { QrCode } from "../../reservation/components/Qrcode";
 
 export function SearchSchedule() {
   return (
     <>
       <Suspense fallback={<SearchScheduleBodySkeleton />}>
         <SearchScheduleBody />
+        <QrCode
+          train_car_type_name="はやぶさ"
+          train_car_number="1"
+          seat_number="1"
+          seat_column="A"
+          code_token="QR-VUEAF6"
+          code_token_text="QR-VUEAF6"
+        />
       </Suspense>
     </>
   );
