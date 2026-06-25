672b290dde80a86639b87ae5f3151d480b4eab6d
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-06-25 17:11:07 +0900

    レスポンシブの調整

--- frontend/src/features/reservation/pages/ReservedTicket.tsx
+++ frontend/src/features/reservation/pages/ReservedTicket.tsx
@@ -2,32 +2,17 @@ import { Suspense } from "react";
 import { ReservedTicketInfo } from "../components/ReservedTicketInfo/ReservedTicketInfo";
 import { ReservedTicketInfoSkeleton } from "../components/ReservedTicketInfo/ReservedTicketInfoSkeleton";
 import { useReservedTickets } from "../hooks/useReservedTickets";
-import { GoHome } from "react-icons/go";
-import { useNavigate } from "react-router-dom";
 
 export function ReservedTicket() {
   const { reservedTickets } = useReservedTickets(
     "37ce698d-1780-4f43-964c-f75d816b87e3",
   ); /*TODO:パラメータを動的にする*/
 
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="flex flex-col items-center gap-4 w-full p-4">
         {/* TODO: 戻るボタンを作る */}
-        <div className="w-full md:w-7/10">
-          <button
-            onClick={() => {
-              navigate("/searchSchedule");
-            }}
-            className="flex ml-auto items-center gap-2 px-4 border-2 rounded-xl border-primary-light cursor-pointer"
-          >
-            <div>ホーム</div>
-            <GoHome />
-          </button>
-        </div>
-        <div className="w-full md:w-7/10">
+        <div className="w-full md:w-7/10 min-w-[360px]">
           <Suspense fallback={<ReservedTicketInfoSkeleton />}>
             <ReservedTicketInfo ticketInfo={reservedTickets} />
           </Suspense>
