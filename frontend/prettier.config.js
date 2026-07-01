d474650e52fa6c14b9c33ceafe9ab465f817ff44
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-01 13:20:11 +0900

    微調整

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -4,13 +4,13 @@
  */
 
 const config = {
-  semi: true,
-  singleQuote: true,
-  tabWidth: 4,
-  trailingComma: "all",
-  endOfLine: "lf",
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    endOfLine: 'lf',
 
-  plugins: ["prettier-plugin-tailwindcss"],
+    plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
