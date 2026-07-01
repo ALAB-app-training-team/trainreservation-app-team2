42389a2b3893a95f17fb51ca8ee8375f6f9b338d
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-07-01 10:34:59 +0900

    レビュー指摘対応修正漏れ

--- backend/src/test/java/com/alab/shinkansendego/features/schedule/controllers/PurchaseControllerTest.java
+++ backend/src/test/java/com/alab/shinkansendego/features/schedule/controllers/PurchaseControllerTest.java
@@ -40,7 +40,7 @@ public class PurchaseControllerTest {
 
     @Test
     @DisplayName("購入情報・購入座席情報を挿入できる")
-    void insertPurchaseSeats_withValidReserveRequestDto_return201AndInsertPurchaseId() throws Exception {
+    void insertPurchase_withValidReserveRequestDto_return201AndInsertPurchaseId() throws Exception {
         ReserveRequestDto request = new ReserveRequestDto(
                 "Test01", LocalDate.now(), "Test0", "Test1", List.of(
                 new ReserveRequestDto.SelectedSeatDto("E5SER01", "SEAT01001"),
