f75493cad1b7a87dc8cc0c54683320bdf69b059e
hashimoto.tatsuya <hashimoto.tatsuya@wing.sysrdc.com>
2026-07-08 14:25:31 +0900

    add until Faclock

--- frontend/src/features/reservation/components/ReservationList/ReservationSelectItem.tsx
+++ frontend/src/features/reservation/components/ReservationList/ReservationSelectItem.tsx
@@ -1,9 +1,10 @@
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { BsQrCode } from 'react-icons/bs';
-import { FaSearch } from 'react-icons/fa';
+import { FaClock, FaSearch } from 'react-icons/fa';
 import { IoTrashOutline } from 'react-icons/io5';
 import { LuTicket } from 'react-icons/lu';
+import { MdAirlineSeatReclineExtra } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 
 import { ReservedSeats } from '@/features/reservation/components/ReservedSeats';
@@ -33,7 +34,7 @@ export function ReservationSelectItem({ details }: ReservationSelectItemProps) {
         <div className="border-primary-light flex flex-col gap-2 rounded-2xl border-2 p-8">
             <div className="flex-col">
                 <div className="flex">
-                    <div className="flex grow-2 items-center gap-2 text-xl font-bold">
+                    <div className="flex grow-2 items-center gap-2">
                         <LuTicket />
                         <label>{details.trainTypeName}</label>
                     </div>
@@ -47,7 +48,7 @@ export function ReservationSelectItem({ details }: ReservationSelectItemProps) {
                         </div>
                     )}
                 </div>
-                <div className="flex py-2">
+                <div className="flex py-2 text-xl font-bold">
                     <label>
                         {details.departureStationName} →{' '}
                         {details.arrivalStationName}
@@ -56,24 +57,32 @@ export function ReservationSelectItem({ details }: ReservationSelectItemProps) {
             </div>
             <div className="flex justify-between">
                 <div className="flex w-full flex-col items-start">
-                    <label>出発</label>
+                    <div className="flex grow-2 items-center gap-2">
+                        <FaClock />
+                        <label>出発</label>
+                    </div>
                     <label className="text-xl font-bold">
                         {dayjs(details.rideDate).format('YYYY年MM月DD日')}{' '}
                     </label>
-                    <label>
+                    <label className="text-xl font-bold">
                         {dayjs(details.departureTime, 'HH:mm:ss').format(
                             'HH:mm',
                         )}
                     </label>
                 </div>
             </div>
-            <div className="border-primary/20 flex border-b-2 py-2">
-                <div className="flex flex-col items-start gap-2 self-start">
-                    <ReservedSeats
-                        id="reservationList"
-                        title="座席"
-                        seats={details.reservedSeats}
-                    />
+            <div className="border-primary/20 border-b-2 py-2">
+                <div className="flex items-start">
+                    <MdAirlineSeatReclineExtra className="me-2 mt-1 flex-shrink-0" />
+                    <div className="-ml-8 flex w-full flex-col items-start gap-2 self-start">
+                        <div className="w-full pl-8">
+                            <ReservedSeats
+                                id="reservationList"
+                                title="座席"
+                                seats={details.reservedSeats}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
