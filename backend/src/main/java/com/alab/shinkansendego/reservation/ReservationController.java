052133b7d0ea69bc98b5ccfe6a896a34456ba57c
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-07-08 15:01:43 +0900

    BEの修正を元に戻す

--- backend/src/main/java/com/alab/shinkansendego/reservation/ReservationController.java
+++ backend/src/main/java/com/alab/shinkansendego/reservation/ReservationController.java
@@ -1,6 +1,5 @@
 package com.alab.shinkansendego.reservation;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +21,8 @@ public class ReservationController {
     }
 
     @GetMapping(path = "api/shinkansen-reservationlist")
-    public ResponseEntity<List<ReservationResponseDto>> getReservationList(@Valid ReservationListRequestDto request) {
-        List<ReservationResponseDto> response = reservationService.getReservationList(request);
+    public ResponseEntity<List<ReservationResponseDto>> getReservationList() {
+        List<ReservationResponseDto> response = reservationService.getReservationList();
         return ResponseEntity.ok(response);
     }
 
