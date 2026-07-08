9178a11712bd2f42ebd294d7a0e85d7125068e71
yasuda.risa <ryasuda@jeis-bi.net>
2026-07-08 11:24:28 +0900

    型名を修正

--- frontend/src/features/schedule/types/ReserveRequestDto.ts
+++ frontend/src/features/schedule/types/ReserveRequestDto.ts
@@ -1,4 +1,4 @@
-type SelectedSeats = {
+type SelectedSeat = {
     trainCarCd: string;
     seatCd: string;
 };
@@ -8,7 +8,7 @@ export type ReserveRequestDto = {
     rideDate: string;
     departureStationCd: string;
     arrivalStationCd: string;
-    seats: SelectedSeats[];
+    seats: SelectedSeat[];
     reserverName: string;
     reserverMail: string;
     paymentToken: string;
