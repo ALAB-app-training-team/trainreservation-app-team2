e7f13273d9085ff952bb8c0cb1563372dbcfd489
yasuda.risa <ryasuda@jeis-bi.net>
2026-07-09 14:25:15 +0900

    paymentControllerをcors全開に変更

--- backend/src/main/java/com/alab/shinkansendego/payment/PaymentController.java
+++ backend/src/main/java/com/alab/shinkansendego/payment/PaymentController.java
@@ -4,15 +4,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/payments")
+@CrossOrigin(origins = "*")
 public class PaymentController {
     @Autowired
     public PaymentController() {
