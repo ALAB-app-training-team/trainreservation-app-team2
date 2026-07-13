3670f2bd3a31834737e17381da6001a209bb02ab
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-13 14:13:53 +0900

    toriaezu

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
     webServer: [
         {
             command: 'npm run dev',
-            url: 'http://127.0.0.1:5173',
+            url: 'http://localhost:5173',
             reuseExistingServer: !process.env.CI,
         },
         // {
