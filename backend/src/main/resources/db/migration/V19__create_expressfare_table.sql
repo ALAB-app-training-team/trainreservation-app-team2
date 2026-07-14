30de5235c87a349f8bd972dec1c95743b64625c2
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-07-14 17:45:28 +0900

    マイグレーションの作成

---
+++ backend/src/main/resources/db/migration/V19__create_expressfare_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE M_ExpressFareKm
+(
+    express_fare_cd VARCHAR(5) NOT NULL PRIMARY KEY,
+    min_km          INT        NOT NULL,
+    max_km          INT        NOT NULL,
+    express_fare    INT        NOT NULL
+);
+
+INSERT INTO M_ExpressFareKm (express_fare_cd, min_km, max_km, express_fare)
+VALUES ('EF001', 0, 100, 1870),
+       ('EF002', 100, 150, 2300),
+       ('EF003', 150, 200, 2640),
+       ('EF004', 200, 300, 3530),
+       ('EF005', 300, 400, 4300),
+       ('EF006', 400, 500, 4840),
+       ('EF007', 500, 600, 5170),
+       ('EF008', 600, 700, 5540),
+       ('EF009', 700, 800, 6070);
+
+
---
+++ backend/src/main/resources/db/migration/V21__alter_reservedseat_table.sql
@@ -0,0 +1,11 @@
+ALTER TABLE T_ReservedSeat
+    ADD COLUMN seat_fare INT;
+
+UPDATE T_ReservedSeat
+SET seat_fare = 10000
+WHERE seat_fare IS NULL;
+
+ALTER TABLE T_ReservedSeat
+    ALTER COLUMN seat_fare SET NOT NULL;
+
+
