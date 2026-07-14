716c51296de533dddb672aa9a86612a91b189cc3
TerasakiKaho <kterasaki@jeisryokai.onmicrosoft.com>
2026-07-14 15:18:46 +0900

    S7-PBI⑤_予約一覧画面を更新したら情報が更新される (#198)
    * 不要なinitialdataを削除
    
    * スケルトンのセレクトタブ修正
    
    ---------
    
    Co-authored-by: terasaki.kaho <terasaki.kaho@wing.sysrdc.com>

--- frontend/src/features/reservation/components/ReservationList/ReservationListBodySkeletons.tsx
+++ frontend/src/features/reservation/components/ReservationList/ReservationListBodySkeletons.tsx
@@ -1,10 +1,20 @@
+import { CiCalendar } from 'react-icons/ci';
+import { RiGroupLine } from 'react-icons/ri';
+
 export function ReservationListBodySkeleton() {
     return (
         <>
-            <div className="flex justify-center">
-                <div className="m-8 flex min-h-96 w-full max-w-5xl flex-col gap-4">
-                    <svg className="h-full w-full animate-pulse rounded-2xl bg-gray-300" />
+            <div className="mx-auto flex max-w-4xl flex-col gap-8 p-4">
+                <h1 className="!m-0 text-left !text-3xl">予約確認</h1>
+                <div className="bg-primary/8 flex gap-6 rounded-3xl p-2">
+                    <button className="flex w-full items-center justify-center gap-2 rounded-3xl bg-white px-6 py-2 font-bold shadow transition">
+                        <CiCalendar />- （-）
+                    </button>
+                    <button className="flex w-full items-center justify-center gap-2 rounded-3xl px-6 py-2 transition">
+                        <RiGroupLine />- （-）
+                    </button>
                 </div>
+                <svg className="w-full animate-pulse rounded-2xl bg-gray-300" />
             </div>
         </>
     );
