3ee488095b240a852f3787b38f38006cdbd0cb74
Ayaka Nihonyanagi <anihonyanagi@jeis-bi.net>
2026-07-14 13:26:44 +0900

    S6_PBI⑦e2eテストを導入する (#183)
    * とりあえずいれる
    
    * とりあえずいれる
    
    * .gitignoreの追加
    
    * .gitignoreで追跡したくないものを削除
    
    * .gitignoreで追跡したくないものを削除
    
    * 遷移のテストと画面のテストを追加
    
    * 画面遷移のテストを座席が予約されてても、あいてる席を探してとる形に修正
    
    * 見た目のデグレテストを全画面で実施
    
    * pomの追加、それに伴い、navigateの修正
    
    * 最新のmainに合わせて修正
    
    * 検索ページのテストコード追加
    
    * 検索ページのテストコード追加
    
    * githubactionがうまく動いていなかったのでもろもろ修正
    
    * githubactionがうまく動いていなかったのでもろもろ修正
    
    * 足りていないimportを追加
    
    * ファイルアクセスのコマンド追加
    
    * githubactionがうまく動いていなかったのでもろもろ修正
    
    * webserverのデフォルトURLを変更してみる
    
    * おためし
    
    * localymlで動かないかなっておためし
    
    * dockerたてるコマンド追加してみた
    
    * dockerたてるコマンド追加してみた
    
    * dockerたてるコマンド追加してみた
    
    * stascktrace追加してみた
    
    * ポート番号をかえてみる
    
    * JDKの場所変えてみる
    
    * JDKの場所もどす
    
    * --infoでさらに詳しく出るらしいですよ
    
    * --infoでさらに詳しく出るらしいですよ
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * toriaezu
    
    * 描画を待ってから、スクショをとるように修正
    
    * 見た目のリグレッションテストで動的に変更になる部分にマスクを設定
    
    * 10%ちがくても許すよにする
    
    * visualテストえぐいので後回しにしたい…
    
    * エラーがないことを確認するコードを追加
    
    * 絶対パス設定追加、絶対パス化、スナップショットの削除
    
    * 自動テストのjob名変更
    
    ---------
    
    Co-authored-by: 二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>

---
+++ frontend/tests/navigate.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+import { ScheduleSearchPage } from '@tests/pages/ScheduleSearch/ScheduleSearchPage';
+import { SelectSeatPage } from '@tests/pages/SelectSeat/SelectSeatPage';
+import { ReservationListPage } from '@tests/pages/ReservationList/ReservationListPage';
+import { ReservedTicketPage } from '@tests/pages/ReservedTicket/ReservedTicketPage';
+import { ReservationGuestLoginPage } from '@tests/pages/ReservationGuestLogin/ReservationGuestLoginPage';
+
+test('navigate-検索～予約確認', async ({ page }) => {
+    const scheduleSearchPage = new ScheduleSearchPage(page);
+    const selectSeatPage = new SelectSeatPage(page);
+    const reservationGuestLogin = new ReservationGuestLoginPage(page);
+    const reservationListPage = new ReservationListPage(page);
+    const reservedTicketPage = new ReservedTicketPage(page);
+
+    await scheduleSearchPage.goto();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.clickBackButton();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.selectSeat();
+    await selectSeatPage.inputResererInfo();
+    await selectSeatPage.inputCardInfo();
+    await selectSeatPage.clickReseveButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    await expect(
+        page.getByText(
+            'エラーが発生しました。しばらくしてから再度お試しください。',
+        ),
+    ).toBeHidden();
+
+    await reservationGuestLogin.goto();
+    await expect(page).toHaveURL('/reservationGuestLogin');
+    await reservationGuestLogin.inputGuestLoginInfo();
+    await reservationGuestLogin.clickGuestLoginButton();
+    await expect(page).toHaveURL('/reservationList');
+    await reservationListPage.clickTicketButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    await reservedTicketPage.clickBackButton();
+    await expect(page).toHaveURL('/reservationList');
+    await expect(
+        page.getByText(
+            'エラーが発生しました。しばらくしてから再度お試しください。',
+        ),
+    ).toBeHidden();
+});
+
+test('navigate-header', async ({ page }) => {
+    const reservationGuestLoginPage = new ReservationGuestLoginPage(page);
+    const scheduleSearchPage = new ScheduleSearchPage(page);
+
+    await reservationGuestLoginPage.goto();
+    await expect(page).toHaveURL('/reservationGuestLogin');
+    await reservationGuestLoginPage.header.goToSchduleSearchBySystemName();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.header.gotoReservationGuestLogin();
+    await expect(page).toHaveURL('/reservationGuestLogin');
+    await reservationGuestLoginPage.header.gotoScheduleSearch();
+    await expect(page).toHaveURL('/scheduleSearch');
+});
---
+++ frontend/tests/pages/ScheduleSearch/ScheduleSearchPage.ts
@@ -0,0 +1,38 @@
+import { type Locator, type Page } from '@playwright/test';
+import { Header } from '@tests/pages/shared/Header';
+
+export class ScheduleSearchPage {
+    readonly page: Page;
+    readonly header: Header;
+    readonly detailButton: Locator;
+    readonly departureStation: Locator;
+    readonly arrivalStation: Locator;
+    readonly date: Locator;
+    readonly time: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.header = new Header(page);
+        this.detailButton = page.getByRole('button', { name: '詳細を見る' });
+        this.departureStation = page.getByLabel('乗車駅');
+        this.arrivalStation = page.getByLabel('降車駅');
+        this.date = page.getByRole('textbox', { name: '出発日' });
+        this.time = page.getByRole('textbox', { name: '出発時刻' });
+    }
+
+    async goto() {
+        await this.page.goto('/');
+    }
+
+    async clickDetailButton() {
+        await this.detailButton.first().click();
+    }
+
+    async openDepartureStationDropdown() {
+        await this.departureStation.first().click();
+    }
+
+    async openArrivalStationDropdown() {
+        await this.arrivalStation.first().click();
+    }
+}
