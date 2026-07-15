0b5d693cdf1c031fff7e705d175120dde0791a4c
yasuda.risa <ryasuda@jeis-bi.net>
2026-07-15 14:13:47 +0900

    シートマップ画面の画面テストひな型

---
+++ frontend/tests/pages/SelectSeat/selectSeatPage.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('http://localhost:5173/scheduleSearch');
+    await page.getByRole('button', { name: '詳細を見る' }).first().click();
+    await page.getByRole('button', { name: '2A', exact: true }).click();
+    await page.getByRole('button', { name: '2B', exact: true }).click();
+    await page.getByRole('button').filter({ hasText: /^$/ }).click();
+    await page.getByRole('button', { name: '2A', exact: true }).click();
+    await page.getByRole('button', { name: '2B', exact: true }).click();
+    await page.getByRole('button', { name: 'グリーン車' }).click();
+    await page.getByRole('button', { name: '2A', exact: true }).click();
+    await page.getByRole('button', { name: '2B', exact: true }).click();
+    await page.getByRole('button', { name: 'グランクラス' }).click();
+    await page.getByRole('button', { name: '2A' }).click();
+    await page.getByRole('button', { name: '2B' }).click();
+    await page.getByRole('textbox', { name: '購入者氏名' }).click();
+    await page.getByRole('textbox', { name: '購入者氏名' }).fill('山田 太郎');
+    await page.getByRole('textbox', { name: '購入者氏名' }).press('Tab');
+    await page
+        .getByRole('textbox', { name: 'メールアドレス' })
+        .fill('demo@example.com');
+    await page.getByRole('textbox', { name: 'メールアドレス' }).press('Tab');
+    await page
+        .getByRole('textbox', { name: 'カード番号' })
+        .fill('4111222233334444');
+    await page.getByRole('textbox', { name: 'カード番号' }).press('Tab');
+    await page
+        .getByRole('textbox', { name: 'カード名義人' })
+        .fill('TARO YAMADa');
+    await page.getByRole('textbox', { name: 'カード名義人' }).press('Tab');
+    await page
+        .getByRole('textbox', { name: '有効期限（月/年）' })
+        .fill('12/28');
+    await page.getByRole('textbox', { name: '有効期限（月/年）' }).press('Tab');
+    await page.getByRole('textbox', { name: 'セキュリティコード' }).fill('123');
+    await page.getByRole('button', { name: '予約を確定' }).click();
+    await page.getByRole('button', { name: 'キャンセル' }).click();
+    await page.getByRole('button', { name: '予約を確定' }).click();
+    await page.getByRole('button', { name: '予約を確定' }).click();
+});
