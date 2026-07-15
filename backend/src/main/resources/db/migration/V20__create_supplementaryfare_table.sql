86a7082c307d962f7d3141110e2bd9d925eaa717
TerasakiKaho <kterasaki@jeisryokai.onmicrosoft.com>
2026-07-15 11:53:44 +0900

    S7-PBI①-1_マイグレーションコード作成 (#200)
    * マイグレーションの作成
    
    * 空行の削除
    
    * alter_tableのファイル削除
    
    * ユニーク設定を追加
    
    ---------
    
    Co-authored-by: terasaki.kaho <terasaki.kaho@wing.sysrdc.com>

---
+++ backend/src/main/resources/db/migration/V20__create_supplementaryfare_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE M_SupplementaryFareKm
+(
+    supplementary_fare_cd VARCHAR(5) NOT NULL PRIMARY KEY,
+    min_km                INT        NOT NULL,
+    max_km                INT        NOT NULL,
+    reserved_fare         INT        NOT NULL,
+    green_fare            INT        NOT NULL,
+    gc_fare               INT        NOT NULL,
+    UNIQUE (min_km, max_km)
+);
+
+INSERT INTO M_SupplementaryFareKm (supplementary_fare_cd, min_km, max_km, reserved_fare, green_fare, gc_fare)
+VALUES ('SF001', 0, 100, 530, 1300, 8300),
+       ('SF002', 100, 200, 530, 2800, 9800),
+       ('SF003', 200, 400, 530, 4190, 11190),
+       ('SF004', 400, 600, 530, 5400, 12400),
+       ('SF005', 600, 700, 530, 5600, 12600),
+       ('SF006', 700, 1000, 530, 6600, 13600);
