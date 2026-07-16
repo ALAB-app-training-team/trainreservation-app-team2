70f3fbebcdbbfb91cced9560abdc4f0fcf1b5935
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-07-16 12:01:06 +0900

    resourceファイルを除く

--- backend/src/main/resources/db/migration/V17__alter_reservation_table.sql
+++ backend/src/main/resources/db/migration/V17__alter_reservation_table.sql
@@ -1,5 +1,5 @@
 ALTER TABLE T_Reservation
-    ADD COLUMN reserver_name VARCHAR(255),
+ADD COLUMN reserver_name VARCHAR(255),
 ADD COLUMN reserver_mail VARCHAR(255),
 ADD COLUMN payment_tracking_id VARCHAR(36);
 
@@ -8,4 +8,4 @@ SET payment_tracking_id = ''
 WHERE payment_tracking_id IS NULL;
 
 ALTER TABLE T_Reservation
-    ALTER COLUMN payment_tracking_id SET NOT NULL;
+ALTER COLUMN payment_tracking_id SET NOT NULL;
