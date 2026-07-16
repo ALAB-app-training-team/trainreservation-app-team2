2ab2c7fe506c1c11966ceec1435801dc1d962f21
yasuda.risa <ryasuda@jeis-bi.net>
2026-07-16 09:28:58 +0900

    テスト作成中

--- frontend/tests/pages/SelectSeat/selectSeatPage.spec.ts
+++ frontend/tests/pages/SelectSeat/selectSeatPage.spec.ts
@@ -1,49 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { ScheduleSearchPage } from '@tests/pages/ScheduleSearch/ScheduleSearchPage';
 import { SelectSeatPage } from '@tests/pages/SelectSeat/SelectSeatPage';
-import { ReservationListPage } from '@tests/pages/ReservationList/ReservationListPage';
-import { ReservedTicketPage } from '@tests/pages/ReservedTicket/ReservedTicketPage';
-import { ReservationGuestLoginPage } from '@tests/pages/ReservationGuestLogin/ReservationGuestLoginPage';
-
-test('test', async ({ page }) => {
-    const scheduleSearchPage = new ScheduleSearchPage(page);
-    const selectSeatPage = new SelectSeatPage(page);
-
-    await scheduleSearchPage.goto();
-    await expect(page).toHaveURL('/scheduleSearch');
-    await scheduleSearchPage.clickDetailButton();
-
-    await page.getByRole('textbox', { name: '購入者氏名' }).fill('山田 太郎');
-    await page
-        .getByRole('textbox', { name: 'メールアドレス' })
-        .fill('demo@example.com');
-    await page
-        .getByRole('textbox', { name: 'カード番号' })
-        .fill('4111222233334444');
-    await page
-        .getByRole('textbox', { name: 'カード名義人' })
-        .fill('TARO YAMADa');
-    await page
-        .getByRole('textbox', { name: '有効期限（月/年）' })
-        .fill('12/28');
-    await page.getByRole('textbox', { name: 'セキュリティコード' }).fill('123');
-    await page.getByRole('button', { name: '予約を確定' }).click();
-    await page.getByRole('button', { name: 'キャンセル' }).click();
-    await page.getByRole('button', { name: '予約を確定' }).click();
-    await page.getByRole('button', { name: '予約を確定' }).click();
-
-    await selectSeatPage.selectSeat();
-    await selectSeatPage.inputResererInfo();
-    await selectSeatPage.inputCardInfo();
-    await selectSeatPage.clickReseveButton();
-    await selectSeatPage.clickConfirmButton();
-    await expect(page).toHaveURL('/reservedTicket');
-    await expect(
-        page.getByText(
-            'エラーが発生しました。しばらくしてから再度お試しください。',
-        ),
-    ).toBeHidden();
-});
 
 test('ゴミ箱ボタンを押すと、選択した座席が解除される', async ({ page }) => {
     const scheduleSearchPage = new ScheduleSearchPage(page);
@@ -106,11 +63,24 @@ test('座席を6席選択すると、それ以上選択できない', async ({ p
 
 test('バリデーション？', async ({ page }) => {
     const scheduleSearchPage = new ScheduleSearchPage(page);
-    const selectSeatPage = new SelectSeatPage(page);
 
     await scheduleSearchPage.goto();
     await expect(page).toHaveURL('/scheduleSearch');
     await scheduleSearchPage.clickDetailButton();
+    await page.getByRole('textbox', { name: '購入者氏名' }).fill('山田 太郎');
+    await page
+        .getByRole('textbox', { name: 'メールアドレス' })
+        .fill('demo@example.com');
+    await page
+        .getByRole('textbox', { name: 'カード番号' })
+        .fill('4111222233334444');
+    await page
+        .getByRole('textbox', { name: 'カード名義人' })
+        .fill('TARO YAMADa');
+    await page
+        .getByRole('textbox', { name: '有効期限（月/年）' })
+        .fill('12/28');
+    await page.getByRole('textbox', { name: 'セキュリティコード' }).fill('123');
 });
 
 test('予約確定', async ({ page }) => {
