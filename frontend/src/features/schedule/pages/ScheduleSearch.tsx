0a4bcf0c4b626bf4c489c76ce1fb8d8267e1e7ec
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-07-17 12:42:58 +0900

    予約完了時にセッション削除

--- frontend/src/features/schedule/pages/ScheduleSearch.tsx
+++ frontend/src/features/schedule/pages/ScheduleSearch.tsx
@@ -1,9 +1,15 @@
-import { Suspense } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { Suspense, useEffect } from 'react';
 
 import { ScheduleSearchBody } from '@/features/schedule/components/ScheduleSearchBody/ScheduleSearchBody';
 import { ScheduleSearchBodySkeleton } from '@/features/schedule/components/ScheduleSearchBody/ScheduleSearchBodySkeleton';
+import { removeGuestReservation } from '@/shared/utils/RemoveGuestReservation';
 
 export function ScheduleSearch() {
+    const queryClient = useQueryClient();
+    useEffect(() => {
+        removeGuestReservation(queryClient);
+    }, []);
     return (
         <>
             <Suspense fallback={<ScheduleSearchBodySkeleton />}>
