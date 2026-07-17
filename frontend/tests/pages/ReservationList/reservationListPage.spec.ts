d7389cd0a5eda1190373a94479876dbdab810b02
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-17 18:22:00 +0900

    そしたらimportもいらないじゃんね…

--- frontend/tests/pages/ReservationList/reservationListPage.spec.ts
+++ frontend/tests/pages/ReservationList/reservationListPage.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from '@playwright/test';
 import { test } from '@tests/fixtures';
 import { ReservationListPage } from '@tests/pages/ReservationList/ReservationListPage';
-import { ReservationGuestLoginPage } from '@tests/pages/ReservationGuestLogin/ReservationGuestLoginPage';
 
 test('お支払い合計が正しく表示されていること', async ({
     page,
