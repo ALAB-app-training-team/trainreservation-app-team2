2449c6ea470b112cc4838e08e5d039a57da5be71
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-17 18:13:38 +0900

    fixture追加

--- frontend/tests/pages/ReservationList/reservationListPage.spec.ts
+++ frontend/tests/pages/ReservationList/reservationListPage.spec.ts
@@ -1,44 +1,19 @@
-import { expect, test } from '@playwright/test';
-import { ScheduleSearchPage } from '@tests/pages/ScheduleSearch/ScheduleSearchPage';
-import { SelectSeatPage } from '@tests/pages/SelectSeat/SelectSeatPage';
+import { expect } from '@playwright/test';
+import { test } from '@tests/fixtures';
 import { ReservationListPage } from '@tests/pages/ReservationList/ReservationListPage';
 import { ReservationGuestLoginPage } from '@tests/pages/ReservationGuestLogin/ReservationGuestLoginPage';
 
-test('お支払い合計が正しく表示されていること', async ({ page }) => {
+test('お支払い合計が正しく表示されていること', async ({
+    page,
+    createReservation,
+    guestLogin,
+}) => {
     const reservationGuestLogin = new ReservationGuestLoginPage(page);
     const reservationListPage = new ReservationListPage(page);
-    const scheduleSearchPage = new ScheduleSearchPage(page);
-    const selectSeatPage = new SelectSeatPage(page);
 
-    await scheduleSearchPage.goto();
-    await expect(page).toHaveURL('/scheduleSearch');
-    await scheduleSearchPage.clickDetailButton();
-    await expect(page).toHaveURL('/selectSeat');
-    await selectSeatPage.clickBackButton();
-    await expect(page).toHaveURL('/scheduleSearch');
-    await scheduleSearchPage.clickDetailButton();
-    await expect(page).toHaveURL('/selectSeat');
-    await selectSeatPage.selectSeat();
-    await selectSeatPage.inputResererInfo();
-    await selectSeatPage.inputCardInfo();
-    await selectSeatPage.clickReseveButton();
-    await selectSeatPage.clickCancelButton();
-    await selectSeatPage.clickReseveButton();
-    await selectSeatPage.clickConfirmButton();
-    await expect(page).toHaveURL('/reservedTicket');
-    await expect(
-        page.getByText(
-            'エラーが発生しました。しばらくしてから再度お試しください。',
-        ),
-    ).toBeHidden();
-
-    await reservationGuestLogin.goto();
-    await expect(page).toHaveURL('/reservationGuestLogin');
-    await reservationGuestLogin.inputGuestLoginInfo();
-    await reservationGuestLogin.clickGuestLoginButton();
+    await createReservation();
+    await guestLogin();
     await expect(page).toHaveURL('/reservationList');
-    await page.goto('/reservationList');
-    await page.waitForLoadState('networkidle');
     await expect(reservationListPage.totalFareElement.first()).toBeVisible();
     await expect(reservationListPage.totalFareElement.first()).toContainText(
         '2,600',
