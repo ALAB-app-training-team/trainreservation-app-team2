5d9fb0737adb26e044f191fac961bba63f599f9f
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-07-24 11:57:18 +0900

    scheduleServiceTestを修正

--- backend/src/main/java/com/alab/shinkansendego/sectionkm/SectionKmRepository.java
+++ backend/src/main/java/com/alab/shinkansendego/sectionkm/SectionKmRepository.java
@@ -1,20 +1,13 @@
 package com.alab.shinkansendego.sectionkm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SectionKmRepository extends JpaRepository<SectionKmEntity, String> {
-    @Query("SELECT sk.sectionCd FROM SectionKmEntity sk WHERE sk.startStationCd = :startStationCd")
-    List<String> findSectionCdByStartStationCd(String startStationCd);
-
     List<SectionKmEntity> findByStartStationCd(String startStationCd);
-
-    @Query("SELECT sk.sectionCd FROM SectionKmEntity sk WHERE sk.goalStationCd = :goalStationCd")
-    List<String> findSectionCdByGoalStationCd(String goalStationCd);
 
     List<SectionKmEntity> findByGoalStationCd(String goalStationCd);
 
