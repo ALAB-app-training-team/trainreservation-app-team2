fa4f26df3ca4bf08b06dabe46d779adc50ae41bd
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-24 15:37:21 +0900

    Controllerのテストコード作成

--- backend/src/test/java/com/alab/shinkansendego/account/AccountControllerTest.java
+++ backend/src/test/java/com/alab/shinkansendego/account/AccountControllerTest.java
@@ -1,6 +1,7 @@
 package com.alab.shinkansendego.account;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,13 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,12 +43,20 @@ public class AccountControllerTest {
         when(service.login(account.getMail(), rawPassword)).thenReturn(account);
 
         LoginRequestDto loginRequestDto = new LoginRequestDto(account.getMail(), rawPassword);
-        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "login")
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(loginRequestDto)))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.name").value(account.getName()));
+            .andExpect(jsonPath("$.name").value(account.getName()))
+            .andExpect(jsonPath("$.password").value(account.getPassword()))
+            .andReturn();
 
+        HttpSession session = result.getRequest().getSession(false);
+        assertNotNull(session, "セッションが作成されていること");
+        AccountEntity accountInSession = (AccountEntity) session.getAttribute("LOGIN_ADMIN");
+        assertNotNull(accountInSession, "LOGIN_ADMINという名前でセッションが保存されていること");
+        assertEquals(accountInSession.getName(), account.getName());
+        assertEquals(accountInSession.getPassword(), account.getPassword());
         // セッションが取得？できていること
         // AccountEntityが取得できていること
     }
