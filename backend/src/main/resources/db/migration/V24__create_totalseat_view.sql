852b433f84be7b73ccb23863281fbc3e63c22cbd
hashimoto.tatsuya <hashimoto.tatsuya@wing.sysrdc.com>
2026-07-27 17:04:34 +0900

    レビュー指摘反映

--- backend/src/main/resources/db/migration/V24__create_totalseat_view.sql
+++ backend/src/main/resources/db/migration/V24__create_totalseat_view.sql
@@ -1,6 +1,5 @@
 CREATE OR REPLACE VIEW V_TotalSeat AS
 SELECT
-    'TOTAL_' || TO_CHAR(ROW_NUMBER() OVER (ORDER BY tc.train_series_cd), 'FM000') AS total_seat_cd,
     tc.train_series_cd,
     COALESCE(COUNT(CASE WHEN tct.train_car_type_cd = 'CAR01' THEN 1 END), 0) AS reserved_total,
     COALESCE(COUNT(CASE WHEN tct.train_car_type_cd = 'CAR02' THEN 1 END), 0) AS green_total,
