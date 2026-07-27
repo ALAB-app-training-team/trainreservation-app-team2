1a8118a1de7635538b3280b2dd946771df019259
yasuda.risa <ryasuda@jeis-bi.net>
2026-07-27 12:00:32 +0900

    ログアウトボタンをつけた

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -1,6 +1,7 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const BASE_URL = `${apiBaseUrl}/api`;
 const LOGIN = `${BASE_URL}/login`;
+const LOGOUT = `${BASE_URL}/logout`;
 const PAYMENT = `${BASE_URL}/payments`;
 const RESERVATION = `${BASE_URL}/reservations`;
 const SCHEDULES = `${BASE_URL}/schedules`;
@@ -10,6 +11,7 @@ const TRAINCARS = `${BASE_URL}/traincars`;
 
 export const ENDPOINTS = {
     LOGIN: () => LOGIN,
+    LOGOUT: () => LOGOUT,
     PAYMENT_TOKEN: () => `${PAYMENT}/tokens`,
     RESERVATION: (id?: string) => (id ? `${RESERVATION}/${id}` : RESERVATION),
     SCHEDULES_SEARCH: () => SCHEDULES,
--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -1,12 +1,16 @@
-import { useContext } from 'react';
+import axios from 'axios';
+import { useContext, useState } from 'react';
 import { BsTrainFreightFrontFill } from 'react-icons/bs';
-import { NavLink, useLocation } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
+import { ENDPOINTS } from '@/api/routes';
 import { authContext } from '@/context/AuthContext';
 
 export function Header() {
     const location = useLocation();
-    const { name } = useContext(authContext);
+    const navigate = useNavigate();
+    const { name, setName } = useContext(authContext);
     const buttons = [
         {
             label: '新幹線を探す',
@@ -26,39 +30,73 @@ export function Header() {
             : []),
     ];
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleMenuOpen = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleLogout = async () => {
+        await axios.post<string>(ENDPOINTS.LOGOUT(), {
+            withCredentials: true,
+        });
+        localStorage.clear();
+        setName(null);
+        handleMenuOpen();
+        navigate('/Login');
+    };
+
     return (
-        <div className="border-primary-light relative flex min-h-16 items-center justify-start gap-6 border-b-2 px-8 py-2">
-            <NavLink
-                to="/"
-                className={({ isActive }) =>
-                    `text-primary flex items-center gap-2 px-4 text-lg font-bold ${
-                        isActive || location.pathname === '/scheduleSearch'
-                            ? 'cursor-default'
-                            : 'cursor-pointer'
-                    }`
-                }
-            >
-                <BsTrainFreightFrontFill />
-                新幹線でGO！
-            </NavLink>
-            <div className="flex flex-1 items-center justify-end gap-4 px-4">
-                {buttons.map((button, index) => (
-                    <NavLink
-                        key={index}
-                        to={button.to}
-                        className={({ isActive }) =>
-                            `rounded-xl px-4 py-3 text-sm font-bold ${
-                                isActive
-                                    ? 'bg-primary cursor-default text-white'
-                                    : 'cursor-pointer'
-                            }`
-                        }
-                    >
-                        {button.label}
-                    </NavLink>
-                ))}
-                {name && <div className="text-sm font-bold">{name}さん</div>}
+        <>
+            <div className="border-primary-light relative flex min-h-16 items-center justify-start gap-6 border-b-2 px-8 py-2">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        `text-primary flex items-center gap-2 px-4 text-lg font-bold ${
+                            isActive || location.pathname === '/scheduleSearch'
+                                ? 'cursor-default'
+                                : 'cursor-pointer'
+                        }`
+                    }
+                >
+                    <BsTrainFreightFrontFill />
+                    新幹線でGO！
+                </NavLink>
+                <div className="flex flex-1 items-center justify-end gap-4 px-4">
+                    {buttons.map((button, index) => (
+                        <NavLink
+                            key={index}
+                            to={button.to}
+                            className={({ isActive }) =>
+                                `rounded-xl px-4 py-3 text-sm font-bold ${
+                                    isActive
+                                        ? 'bg-primary cursor-default text-white'
+                                        : 'cursor-pointer'
+                                }`
+                            }
+                        >
+                            {button.label}
+                        </NavLink>
+                    ))}
+                    {name && (
+                        <div className="relative">
+                            <button
+                                onClick={handleMenuOpen}
+                                className="flex items-center gap-2 text-sm font-bold"
+                            >
+                                <FiMenu />
+                                {name}さん
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
+            {isMenuOpen && (
+                <div className="absolute top-full right-0 z-10 w-[120px] rounded-md bg-white p-2 text-sm font-bold shadow-md">
+                    <button onClick={handleLogout} className="w-full text-left">
+                        ログアウト
+                    </button>
+                </div>
+            )}
+        </>
     );
 }
