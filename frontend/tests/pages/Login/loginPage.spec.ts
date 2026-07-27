35dc56722331147235fadc017775e65d4f2edc59
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-27 09:49:52 +0900

    ログインページのテストコードを追加

---
+++ frontend/tests/pages/Login/loginPage.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '@tests/pages/Login/LoginPage';
+
+test('未入力の時にバリデーションメッセージが表示されること', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    loginPage.goto();
+    await expect(page).toHaveURL('/login');
+    await loginPage.clickLoginButton();
+    const message = await loginPage.mailAddress.evaluate(
+        (el: HTMLInputElement) => el.validationMessage,
+    );
+    expect(message).toBe('このフィールドに入力してください。');
+    await expect(page).toHaveURL('/login');
+});
