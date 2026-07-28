cc8cf749ae6e5800cb0e947ccc5542eae2b898fd
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-07-28 16:25:09 +0900

    シングルコォーテーションに変更

--- backend/src/main/resources/db/migration/V26__alter_reservedseatsection_table_for_traincartypecd.sql
+++ backend/src/main/resources/db/migration/V26__alter_reservedseatsection_table_for_traincartypecd.sql
@@ -7,15 +7,15 @@ ALTER TABLE T_ReservedSeatSection
             ON DELETE CASCADE;
 
 UPDATE T_ReservedSeatSection
-    SET train_car_type_cd = "CAR01"
+    SET train_car_type_cd = 'CAR01'
         WHERE train_car_type_cd IS NULL;
 
 UPDATE T_ReservedSeatSection
-    SET train_car_type_cd = "CAR02"
+    SET train_car_type_cd = 'CAR02'
         WHERE train_car_cd IN ('E2SER09', 'E5SER09', 'E6SER01', 'E7SER11', 'E8SER01');
 
 UPDATE T_ReservedSeatSection
-    SET train_car_type_cd = "CAR03"
+    SET train_car_type_cd = 'CAR03'
         WHERE train_car_cd IN ('E5SER10', 'E7SER12');
 
 ALTER TABLE T_ReservedSeatSection
