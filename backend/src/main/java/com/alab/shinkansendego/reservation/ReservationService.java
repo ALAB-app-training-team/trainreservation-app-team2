bb930548e7870f150017ef701eb92f42d2829c67
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-07-29 13:45:39 +0900

    これが認証情報照会なしでキャンセルできる晩

--- backend/src/main/java/com/alab/shinkansendego/reservation/ReservationService.java
+++ backend/src/main/java/com/alab/shinkansendego/reservation/ReservationService.java
@@ -347,7 +347,7 @@ public class ReservationService {
      */
     @Transactional
     public void deleteReservation(UUID reservationId) {
-        UUID accountId = securityUtil.GetAccountId();
+//        UUID accountId = securityUtil.GetAccountId();
         ReservationEntity reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new IllegalArgumentException("Reservation is not found"));
         // ここらへんでアカウントIDと比較してなかったらExceptionを返す
         reservation.setIsDeleted(true);
