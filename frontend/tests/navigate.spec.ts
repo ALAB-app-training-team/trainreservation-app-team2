6b46996b1ac447e4242da3723b0864c270186d94
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-08-04 13:30:05 +0900

    コメントアウトしていたコードを動くように修正

--- frontend/tests/navigate.spec.ts
+++ frontend/tests/navigate.spec.ts
@@ -6,55 +6,93 @@ import { ReservedTicketPage } from '@tests/pages/ReservedTicket/ReservedTicketPa
 import { ReservationGuestLoginPage } from '@tests/pages/ReservationGuestLogin/ReservationGuestLoginPage';
 import { App } from '@tests/pages/shared/App';
 import { LoginPage } from '@tests/pages/Login/LoginPage';
-import { Header } from '@tests/pages/shared/Header';
 import { test } from '@tests/fixtures';
 
-// test('navigate-検索～予約確認', async ({ page }) => {
-//     const scheduleSearchPage = new ScheduleSearchPage(page);
-//     const selectSeatPage = new SelectSeatPage(page);
-//     const reservationGuestLogin = new ReservationGuestLoginPage(page);
-//     const reservationListPage = new ReservationListPage(page);
-//     const reservedTicketPage = new ReservedTicketPage(page);
+test('navigate-ゲストログイン検索～予約確認', async ({ page }) => {
+    const scheduleSearchPage = new ScheduleSearchPage(page);
+    const selectSeatPage = new SelectSeatPage(page);
+    const reservationGuestLoginPage = new ReservationGuestLoginPage(page);
+    const reservedTicketPage = new ReservedTicketPage(page);
 
-//     await login();
-//     await expect(page).toHaveURL('/scheduleSearch');
-//     await scheduleSearchPage.clickDetailButton();
-//     await expect(page).toHaveURL('/selectSeat');
-//     await selectSeatPage.clickBackButton();
-//     await expect(page).toHaveURL('/scheduleSearch');
-//     await scheduleSearchPage.clickDetailButton();
-//     await expect(page).toHaveURL('/selectSeat');
-//     await selectSeatPage.selectSeat();
-//     await selectSeatPage.inputResererInfo();
-//     await selectSeatPage.inputCardInfo();
-//     await selectSeatPage.clickReseveButton();
-//     await selectSeatPage.clickCancelButton();
-//     await selectSeatPage.clickReseveButton();
-//     await selectSeatPage.clickConfirmButton();
-//     await expect(page).toHaveURL('/reservedTicket');
-//     await expect(
-//         page.getByText(
-//             'エラーが発生しました。しばらくしてから再度お試しください。',
-//         ),
-//     ).toBeHidden();
+    await scheduleSearchPage.goto();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.clickBackButton();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.selectSeat();
+    await selectSeatPage.inputResererInfo();
+    await selectSeatPage.inputCardInfo();
+    await selectSeatPage.clickReseveButton();
+    await selectSeatPage.clickCancelButton();
+    await selectSeatPage.clickReseveButton();
+    await selectSeatPage.clickConfirmButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    await reservedTicketPage.clickTicketShareButton();
+    await reservedTicketPage.linkCopyElement.waitFor({ state: 'visible' });
+    await reservedTicketPage.clickLinkCopyButton();
+    await expect(reservedTicketPage.linkCopyElement).toHaveText('コピー完了');
+    const clipboardText = await page.evaluate(async () => {
+        return await navigator.clipboard.readText();
+    });
+    await page.goto(clipboardText);
+    await expect(page).toHaveURL(/\/reservationGuestLogin\?.+/);
+    await reservationGuestLoginPage.inputGuestLoginInfo();
+    await reservationGuestLoginPage.clickGuestLoginButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    await expect(
+        page.getByText(
+            'エラーが発生しました。しばらくしてから再度お試しください。',
+        ),
+    ).toBeHidden();
+});
 
-//     await reservationGuestLogin.goto();
-//     await expect(page).toHaveURL(/\/reservationGuestLogin\?.+/);
-//     await reservationGuestLogin.inputGuestLoginInfo();
-//     await reservationGuestLogin.clickGuestLoginButton();
-//     await expect(page).toHaveURL('/reservedTicket');
-//     await reservationListPage.clickTicketButton();
-//     await expect(page).toHaveURL('/reservedTicket');
-//     await reservedTicketPage.clickBackButton();
-//     await expect(page).toHaveURL('/reservationList');
-//     await expect(
-//         page.getByText(
-//             'エラーが発生しました。しばらくしてから再度お試しください。',
-//         ),
-//     ).toBeHidden();
-//     await logout();
-//     await expect(page).toHaveURL('/login');
-// });
+test('navigate-検索～シートマップからログイン～予約完了', async ({
+    page,
+    logout,
+}) => {
+    const scheduleSearchPage = new ScheduleSearchPage(page);
+    const selectSeatPage = new SelectSeatPage(page);
+
+    await scheduleSearchPage.goto();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.selectSeat();
+    await expect(page.getByText('座席が選択されていません')).toBeHidden();
+    await selectSeatPage.inputCardInfo();
+    await selectSeatPage.clickReseveButton();
+    await selectSeatPage.clickConfirmButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    await expect(
+        page.getByText(
+            'エラーが発生しました。しばらくしてから再度お試しください。',
+        ),
+    ).toBeHidden();
+    await logout();
+    await expect(page).toHaveURL('/login');
+});
+
+test('navigate-ログイン～検索～予約完了', async ({ page, login, logout }) => {
+    const scheduleSearchPage = new ScheduleSearchPage(page);
+    const selectSeatPage = new SelectSeatPage(page);
+    const reservedTicketPage = new ReservedTicketPage(page);
+
+    await login();
+    await expect(page).toHaveURL('/scheduleSearch');
+    await expect(scheduleSearchPage.header.commonUser).toBeVisible();
+    await scheduleSearchPage.clickDetailButton();
+    await expect(page).toHaveURL('/selectSeat');
+    await selectSeatPage.selectSeat();
+    await expect(page.getByText('座席が選択されていません')).toBeHidden();
+    await selectSeatPage.inputCardInfo();
+    await selectSeatPage.clickReseveButton();
+    await selectSeatPage.clickConfirmButton();
+    await expect(page).toHaveURL('/reservedTicket');
+    reservedTicketPage.clickTicketShareButton();
+});
 
 test('navigate-header', async ({ page }) => {
     const reservationGuestLoginPage = new ReservationGuestLoginPage(page);
@@ -137,68 +175,4 @@ test('navigate-reservationGuestLoginのURLに予約IDがない場合、列車検
     apps.expectSessionAlert();
     await reservationGuestLoginPage.gotoEmpty();
     await expect(page).toHaveURL('/scheduleSearch');
-});
-
-test('navigate-検索～シートマップからログイン～予約完了', async ({
-    page,
-    logout,
-}) => {
-    const scheduleSearchPage = new ScheduleSearchPage(page);
-    const selectSeatPage = new SelectSeatPage(page);
-    const loginPage = new LoginPage(page);
-    const header = new Header(page);
-
-    await scheduleSearchPage.goto();
-    await expect(page).toHaveURL('/scheduleSearch');
-    await scheduleSearchPage.clickDetailButton();
-    await expect(page).toHaveURL('/selectSeat');
-    await selectSeatPage.selectSeat();
-    await expect(page.getByText('座席が選択されていません')).toBeHidden();
-    await expect(header.commonUser).toBeHidden();
-    await selectSeatPage.clickLoginButton();
-
-    await expect(page).toHaveURL('/login');
-    await loginPage.inputLoginInfo();
-    await loginPage.clickLoginButton();
-
-    await expect(page).toHaveURL('/selectSeat');
-    await expect(page.getByText('座席が選択されていません')).toBeHidden();
-    await expect(selectSeatPage.loginButton).not.toBeVisible();
-    await selectSeatPage.inputCardInfo();
-    await selectSeatPage.clickReseveButton();
-    await selectSeatPage.clickConfirmButton();
-    await expect(page).toHaveURL('/reservedTicket');
-    await expect(
-        page.getByText(
-            'エラーが発生しました。しばらくしてから再度お試しください。',
-        ),
-    ).toBeHidden();
-    await logout();
-    await expect(page).toHaveURL('/login');
-});
-
-test('navigate-ログイン～検索～予約完了', async ({ page, login, logout }) => {
-    const scheduleSearchPage = new ScheduleSearchPage(page);
-    const selectSeatPage = new SelectSeatPage(page);
-    const header = new Header(page);
-
-    await login();
-    await expect(page).toHaveURL('/scheduleSearch');
-    await expect(header.commonUser).toBeVisible();
-    await scheduleSearchPage.clickDetailButton();
-    await expect(page).toHaveURL('/selectSeat');
-    await expect(selectSeatPage.loginButton).not.toBeVisible();
-    await selectSeatPage.selectSeat();
-    await expect(page.getByText('座席が選択されていません')).toBeHidden();
-    await selectSeatPage.inputCardInfo();
-    await selectSeatPage.clickReseveButton();
-    await selectSeatPage.clickConfirmButton();
-    await expect(page).toHaveURL('/reservedTicket');
-    await expect(
-        page.getByText(
-            'エラーが発生しました。しばらくしてから再度お試しください。',
-        ),
-    ).toBeHidden();
-    await logout();
-    await expect(page).toHaveURL('/login');
 });
