d0d56c5cf94f25b3e280da523de10f291fd4944d
Ayaka Nihonyanagi <anihonyanagi@jeis-bi.net>
2026-08-04 13:11:01 +0900

    S10_PBI⑩リポジトリをJPA自動実装に置き換える schedule (#275)
    * 処理の修正完了
    
    * いったん途中
    
    * テストコード修正
    
    ---------
    
    Co-authored-by: 二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>

--- backend/src/main/java/com/alab/shinkansendego/traintype/TrainTypeEntity.java
+++ backend/src/main/java/com/alab/shinkansendego/traintype/TrainTypeEntity.java
@@ -1,12 +1,17 @@
 package com.alab.shinkansendego.traintype;
 
+import com.alab.shinkansendego.trainSeries.TrainSeriesEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
@@ -21,4 +26,8 @@ public class TrainTypeEntity {
     private String name;
     @Column(name = "train_series_cd")
     private String trainSeriesCd;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(name = "train_series_cd", referencedColumnName = "train_series_cd", insertable = false, updatable = false)
+    private TrainSeriesEntity trainSeries;
 }
