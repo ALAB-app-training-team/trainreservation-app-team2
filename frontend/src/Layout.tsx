85bf153620b42bcc362dd3357e3c84905de06631
Risa Yasuda <ryasuda@jeis-bi.net>
2026-08-04 16:49:00 +0900

    S10-PBI⑤_過去・キャンセルした予約を降順で表示する (#284)
    * 予約一覧の昇順・降順を整備
    
    * 予約一覧のタブの挙動修正
    
    * 予約一覧のヘッダーを追従&ヘッダーが干渉しないように調整
    
    * タブ変更時にスクロールが一番上に戻る

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -6,10 +6,10 @@ export function Layout() {
     return (
         <>
             <div className="flex min-h-screen flex-col">
-                <div className="sticky top-0 z-10 bg-white">
+                <div className="sticky top-0 z-50 bg-white">
                     <Header />
                 </div>
-                <div className="w-full min-w-[375px] flex-1 overflow-x-auto">
+                <div className="w-full min-w-[375px] flex-1">
                     <Outlet />
                 </div>
             </div>
