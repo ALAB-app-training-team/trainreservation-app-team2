ad8e6a722a973cba979713e4e626e21825ca5dd1
yasuda.risa <ryasuda@jeis-bi.net>
2026-08-04 16:05:11 +0900

    Controllerの作成

---
+++ backend/src/main/java/com/alab/shinkansendego/reservedseat/ReservedSeatController.java
@@ -0,0 +1,31 @@
+package com.alab.shinkansendego.reservedseat;
+
+import com.alab.shinkansendego.account.AccountSessionDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "api/reservedseats")
+public class ReservedSeatController {
+    private final ReservedSeatService reservedSeatService;
+
+    @Autowired
+    public ReservedSeatController(ReservedSeatService reservedSeatService) {
+        this.reservedSeatService = reservedSeatService;
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateReservedSeats(@PathVariable("id") UUID reservationId, @AuthenticationPrincipal AccountSessionDto session) {
+        UUID accountId = (session != null) ? session.getId() : null;
+        reservedSeatService.updateReservedSeats(reservationId, accountId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
