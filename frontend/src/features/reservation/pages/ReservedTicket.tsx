8c8311847c54c8ea833cc47d6781f15fb008dfe8
yasuda.risa <ryasuda@jeis-bi.net>
2026-08-13 14:33:16 +0900

    FEを作成

--- frontend/src/features/reservation/pages/ReservedTicket.tsx
+++ frontend/src/features/reservation/pages/ReservedTicket.tsx
@@ -1,10 +1,13 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { Suspense, useEffect, useState } from 'react';
+import { IoTrashOutline } from 'react-icons/io5';
 import { LuArrowLeft } from 'react-icons/lu';
 import { RiGroupLine } from 'react-icons/ri';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import { CompanionModal } from '@/features/reservation/components/CompanionModal';
+import { ReservationRefundConfirmModal } from '@/features/reservation/components/ReservationRefundConfirmModal';
 import { ReservedTicketInfo } from '@/features/reservation/components/ReservedTicketInfo/ReservedTicketInfo';
 import { ReservedTicketInfoSkeleton } from '@/features/reservation/components/ReservedTicketInfo/ReservedTicketInfoSkeleton';
 import { ReservedTicketQrCode } from '@/features/reservation/components/ReservedTicketQrCode/ReservedTicketQrCode';
@@ -22,10 +25,24 @@ export function ReservedTicket() {
     useToastForRedirect();
     const location = useLocation();
     const navigate = useNavigate();
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
     const { reservationId, isBack, guestLogin, isUpdated } = location.state;
-    const { reservedTickets, updateCompanions, isUpdating } =
-        useReservedTickets(reservationId);
-    const { isOpen, handleModalOpen, onRequestClose } = useModal();
+    const {
+        reservedTickets,
+        updateCompanions,
+        isUpdating,
+        handleRefundReservation,
+    } = useReservedTickets(reservationId);
+    const {
+        isOpen: isCompanionsModalOpen,
+        handleModalOpen: handleCompanionsModalOpen,
+        onRequestClose: onCompanionsModalRequestClose,
+    } = useModal();
+    const {
+        isOpen: isRefundConfirmModalOpen,
+        handleModalOpen: handleRefundConfirmModalOpen,
+        onRequestClose: onRefundConfirmModalRequestClose,
+    } = useModal();
     const shareUrl = `${window.location.origin}/reservationGuestLogin?reservationId=${reservationId}`;
     const queryClient = useQueryClient();
     const accountInfo = localStorage.getItem('name');
@@ -45,9 +62,28 @@ export function ReservedTicket() {
         if (isUpdating || isInvalid) return;
         try {
             await updateCompanions(formValues);
-            onRequestClose();
+            onCompanionsModalRequestClose();
         } catch {
             alert(ERROR_MESSAGE.COMPANION);
+        }
+    };
+
+    const handleReservationRefund = async () => {
+        if (isSubmitting) return;
+        setIsSubmitting(true);
+        try {
+            await handleRefundReservation();
+            toast.success('予約を取り消しました');
+            if (accountInfo !== null) {
+                navigate('/reservationList');
+            } else {
+                navigate('/scheduleSearch');
+            }
+        } catch {
+            alert(ERROR_MESSAGE.REFUND_RETRY);
+        } finally {
+            setIsSubmitting(false);
+            onRefundConfirmModalRequestClose();
         }
     };
 
@@ -89,24 +125,46 @@ export function ReservedTicket() {
                 <Suspense fallback={<ReservedTicketInfoSkeleton />}>
                     <ReservedTicketInfo ticketInfo={reservedTickets} />
                 </Suspense>
-                <div className="flex w-full gap-4">
+                <div className="flex w-full flex-col gap-4 md:flex-row">
+                    <button
+                        onClick={handleRefundConfirmModalOpen}
+                        disabled={isSubmitting}
+                        className="border-primary text-primary flex w-full items-center justify-center gap-2 rounded-xl border-2 p-2 text-sm"
+                    >
+                        <IoTrashOutline className="h-4 w-4" />
+                        キャンセル
+                    </button>
                     <TicketShare shareUrl={shareUrl} />
                     <button
-                        onClick={handleModalOpen}
-                        className="bg-primary flex w-full items-center justify-center gap-2 rounded-xl text-sm text-white"
+                        onClick={handleCompanionsModalOpen}
+                        className="bg-primary flex w-full items-center justify-center gap-2 rounded-xl p-2 text-sm text-white"
                     >
                         <RiGroupLine className="h-4 w-4" />
                         <div>同行者に割り当て</div>
                     </button>
                 </div>
             </div>
-            <CustomModal isOpen={isOpen} onRequestClose={onRequestClose}>
+            <CustomModal
+                isOpen={isCompanionsModalOpen}
+                onRequestClose={onCompanionsModalRequestClose}
+            >
                 <CompanionModal
                     isInvalid={isInvalid}
                     setIsInvalid={setIsInvalid}
                     isSubmitting={isUpdating}
                     handleSubmit={handleUpdateCompanions}
                     reservedSeats={reservedTickets.reservedSeats}
+                />
+            </CustomModal>
+            <CustomModal
+                isOpen={isRefundConfirmModalOpen}
+                onRequestClose={onRefundConfirmModalRequestClose}
+            >
+                <ReservationRefundConfirmModal
+                    onClick={handleReservationRefund}
+                    onRequestClose={onRefundConfirmModalRequestClose}
+                    isSubmitting={isSubmitting}
+                    detail={reservedTickets}
                 />
             </CustomModal>
         </>
