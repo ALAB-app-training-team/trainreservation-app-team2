c8dd8a09a597c27272d82481cd68aadefbeff82b
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-08-18 12:03:41 +0900

    ログイン情報修正

--- frontend/tests/navigate.spec.ts
+++ frontend/tests/navigate.spec.ts
@@ -66,12 +66,12 @@ test('navigate-ログイン全機能', async ({ page, login, logout }) => {
     await loginPage.goto();
     await loginPage.clickCreateButton();
     await expect(page).toHaveURL('/accountCreate');
-    await accountCreatePage.inputCreateSecondAccountInfo();
+    await accountCreatePage.inputCreateFirstAccountInfo();
     await accountCreatePage.clickCreateButton();
     await expect(page).toHaveURL('/login');
 
     // 作成したアカウントでログイン
-    await loginPage.fillMailAddress('test@test.com');
+    await loginPage.fillMailAddress('first@test.co.jp');
     await loginPage.fillPassword('Password1');
     await loginPage.clickLoginButton();
     await expect(page).toHaveURL('/scheduleSearch');
