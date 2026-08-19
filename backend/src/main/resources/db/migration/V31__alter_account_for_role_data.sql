ea6e5552432e8215bfd6732f06de788894b7ba7d
yasuda.risa <ryasuda@jeis-bi.net>
2026-08-19 16:58:22 +0900

    パスワードが間違っていたのでマイグレーションファイルの更新

--- backend/src/main/resources/db/migration/V31__alter_account_for_role_data.sql
+++ backend/src/main/resources/db/migration/V31__alter_account_for_role_data.sql
@@ -5,5 +5,5 @@ INSERT INTO T_Account(id, name, mail, password, role)
 VALUES ('d2cdf717-a35c-48d9-828e-0ce86e01d477',
         '管理者',
         'test-admin@test.com',
-        '$2a$10$mhjFweW.YlrIUcc4LhhQE.gWbJLIm5qyLCmHnVgSUKoWnGiS8tJN2',
+        '$2a$10$3NwwTYjUFNuoZ614qbjvzew84tPCZDw2X2JFlhDnQmIow518Z7Jii',
         'ROLE_ADMIN');
