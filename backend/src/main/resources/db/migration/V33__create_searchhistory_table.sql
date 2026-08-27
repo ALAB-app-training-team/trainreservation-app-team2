b284124a2b72119da237e81410090c4dfbcf5a1a
齋藤 幹央 <saito.mikihisa@wing.sysrdc.com>
2026-08-27 18:21:22 +0900

    マイグレーションファイル作成

---
+++ backend/src/main/resources/db/migration/V33__create_searchhistory_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE T_SearchHistory
+(
+    id                      UUID        NOT NULL PRIMARY KEY,
+    account_id              UUID        REFERENCES T_Account(id) ON DELETE CASCADE NOT NULL,
+    date                    DATE        NOT NULL,
+    time                    TIME        NOT NULL,
+    departure_station_cd    VARCHAR(5)  NOT NULL,
+    arrival_station_cd      VARCHAR(5)  NOT NULL,
+    is_arrival_time         BOOLEAN     NOT NULL,
+    insert_timestamp        timestamp   NOT NULL,
+)
