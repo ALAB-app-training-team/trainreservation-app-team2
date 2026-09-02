44ef47a2951f83f0b1470238afc0840d5db75093
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-09-02 11:03:03 +0900

    ローカルでvisualReguressionテストを実行しない

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
     testDir: './tests',
+    /* ローカル実行時はvisualRegressionを除外し、GitHubActionでのみ実行する */
+    testIgnore: process.env.CI ? [] : ['**/visualRegression.spec.ts'],
     snapshotPathTemplate:
         '{testDir}/{testFileDir}/visualRegression.spec.ts-snapshots/{arg}{ext}',
     fullyParallel: false,
