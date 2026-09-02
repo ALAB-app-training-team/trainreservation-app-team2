d05271a3f9d9450219de1b17ee6a9fafbf1e8821
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-09-02 15:17:27 +0900

    first()をつける

--- frontend/tests/pages/ReservedTicket/reservedTicketPage.spec.ts
+++ frontend/tests/pages/ReservedTicket/reservedTicketPage.spec.ts
@@ -71,7 +71,7 @@ test('タイトル表示：予約完了時は予約完了、予約確認のゲ�
     await reservationListPage.clickChangeSeatConfirmButton();
     await expect(page).toHaveURL('/selectSeat');
     await expect(page.getByText('座席が選択されていません')).not.toBeVisible();
-    await selectSeatPage.emptySeat.waitFor({ state: 'visible' });
+    await selectSeatPage.emptySeat.first().waitFor({ state: 'visible' });
     await selectSeatPage.selectSeat();
     await selectSeatPage.clickUpdateButton();
     await selectSeatPage.clickUpdateConfirmButton();
@@ -135,7 +135,7 @@ test('戻るボタン表示有無：予約確認のアカウントログイン�
     await reservationListPage.clickChangeSeatConfirmButton();
     await expect(page).toHaveURL('/selectSeat');
     await expect(page.getByText('座席が選択されていません')).not.toBeVisible();
-    await selectSeatPage.emptySeat.waitFor({ state: 'visible' });
+    await selectSeatPage.emptySeat.first().waitFor({ state: 'visible' });
     await selectSeatPage.selectSeat();
     await selectSeatPage.clickUpdateButton();
     await selectSeatPage.clickUpdateConfirmButton();
