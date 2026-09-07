d5c03799752766101267119afe9a2e5adcfa4361
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-09-07 15:53:59 +0900

    windows.scrollToの修正

--- frontend/src/features/reservation/pages/ReservationList.tsx
+++ frontend/src/features/reservation/pages/ReservationList.tsx
@@ -1,10 +1,13 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 import { ReservationListBody } from '@/features/reservation/components/ReservationList/ReservationListBody';
 import { ReservationListBodySkeleton } from '@/features/reservation/components/ReservationList/ReservationListBodySkeletons';
 import { useToastForRedirect } from '@/shared/hooks/useToastForRedirect';
 
 export function ReservationList() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     useToastForRedirect();
     return (
         <>
