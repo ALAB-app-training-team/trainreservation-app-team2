c80a94773bf14f236e5695edccbd7bf24af565df
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-09-09 09:47:53 +0900

    一応いれておこ

--- frontend/src/features/schedule/components/TrainCars/TrainCars.tsx
+++ frontend/src/features/schedule/components/TrainCars/TrainCars.tsx
@@ -60,7 +60,7 @@ export function TrainCars({
             {filteredCars.length > 0 ? (
                 <>
                     <div
-                        className="flex gap-2 overflow-x-auto"
+                        className="flex scrollbar-thin gap-2 overflow-x-auto"
                         data-testid="train-cars"
                     >
                         {filteredCars.map((car) => (
