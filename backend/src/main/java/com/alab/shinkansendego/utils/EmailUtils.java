705d343e4adf01f78ca097accf9ec26696d42811
柿本周成 <skakimoto@jeisryokai.onmicrosoft.com>
2026-09-09 13:10:56 +0900

    GUESTLOGIN_PATH→TICKET_PATH

--- backend/src/main/java/com/alab/shinkansendego/utils/EmailUtils.java
+++ backend/src/main/java/com/alab/shinkansendego/utils/EmailUtils.java
@@ -15,7 +15,7 @@ public final class EmailUtils {
     }
 
     public static final String LOGIN_PATH = "/login";
-    public static final String GUESTLOGIN_PATH = "/reservationGuestLogin?reservationId=";
+    public static final String TICKET_PATH = "/reservationGuestLogin?reservationId=";
     public static final String SUBJECT = "[予約完了] 予約内容のご案内";
     public static final String CHANGE_SUBJECT = "[予約変更] 予約変更内容のご案内";
     public static final String CANCEL_SUBJECT = "[予約キャンセル] 予約キャンセル内容のご案内";
@@ -166,7 +166,7 @@ public final class EmailUtils {
     }
 
     public static String guestLoginUrlFormatter(String baseUrl, UUID reservationId) {
-        return baseUrl + GUESTLOGIN_PATH + reservationId;
+        return baseUrl + TICKET_PATH + reservationId;
     }
 
     public static String ticketUrlFormatter(String baseUrl, ReservationEmailRequestParams params) {
