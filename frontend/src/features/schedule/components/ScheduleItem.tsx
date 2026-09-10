f26ec0b6faa760e9e2647a37fbad406dbffadbb8
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-09-10 12:00:37 +0900

    リストアイテムのボタン化

--- frontend/src/features/schedule/components/ScheduleItem.tsx
+++ frontend/src/features/schedule/components/ScheduleItem.tsx
@@ -101,91 +101,83 @@ export function ScheduleItem({
     dayjs.extend(customParseFormat);
 
     return (
-        <>
+        <button
+            type="button"
+            onClick={handleSearch}
+            data-testid="schedule"
+            disabled={
+                schedule.reservedSeats === 0 &&
+                schedule.greenSeats === 0 &&
+                schedule.gcSeats === 0
+            }
+            className="border-primary-light group flex w-full flex-row flex-wrap items-center justify-start gap-4 rounded-2xl border-2 p-8 transition-colors duration-200 ease-out enabled:hover:border-primary md:items-center"
+        >
             <div
-                data-testid="schedule"
-                className="border-primary-light flex w-full flex-row flex-wrap items-center justify-start gap-4 rounded-2xl border-2 p-8 md:items-center"
+                data-testid="schedule-train"
+                className="order-1 flex flex-1 gap-4 md:flex-none"
             >
-                <div
-                    data-testid="schedule-train"
-                    className="order-1 flex flex-1 gap-4 md:flex-none"
-                >
-                    <div className="flex items-center">
-                        <TrainIcon trainTypeName={schedule.trainTypeName} />
-                    </div>
-                    {(() => {
-                        const trainTypeName =
-                            schedule.trainTypeName.split(/(\d+)/);
-                        return (
-                            <div className="w-full min-w-20 text-left">
-                                <div className="text-lg font-extrabold">
-                                    {trainTypeName[0]}
-                                </div>
-                                <div className="text-base">
-                                    {trainTypeName[1]}
-                                    {trainTypeName[2]}
-                                </div>
+                <div className="flex items-center">
+                    <TrainIcon trainTypeName={schedule.trainTypeName} />
+                </div>
+                {(() => {
+                    const trainTypeName = schedule.trainTypeName.split(/(\d+)/);
+                    return (
+                        <div className="w-full min-w-20 text-left">
+                            <div className="text-lg font-extrabold">
+                                {trainTypeName[0]}
                             </div>
-                        );
-                    })()}
-                </div>
-                <div className="order-3 flex w-full items-center justify-between gap-4 md:order-2 md:flex-1">
-                    <div className="text-left">
-                        <div
-                            data-testid="schedule-departure-time"
-                            className="text-2xl font-black"
-                        >
-                            {dayjs(schedule.departureTime, 'HH:mm:ss').format(
-                                'HH:mm',
-                            )}
+                            <div className="text-base">
+                                {trainTypeName[1]}
+                                {trainTypeName[2]}
+                            </div>
                         </div>
-                        <div>{departureStationName}</div>
-                    </div>
-                    <div className="text-primary flex flex-1 items-center gap-2">
-                        <div className="border-primary-light relative w-full border-t-4 border-dotted">
-                            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2">
-                                {calculateDuration(
-                                    schedule.departureTime,
-                                    schedule.arrivalTime,
-                                )}
-                            </span>
-                        </div>
-                        <div className="text-2xl">
-                            <FiArrowRight />
-                        </div>
-                    </div>
-                    <div className="text-left">
-                        <div
-                            data-testid="schedule-arrival-time"
-                            className="text-2xl font-black"
-                        >
-                            {dayjs(schedule.arrivalTime, 'HH:mm:ss').format(
-                                'HH:mm',
-                            )}
-                        </div>
-                        <div>{arrivalStationName}</div>
-                    </div>
-                </div>
-                <div className="order-2 flex flex-wrap items-center gap-2 md:order-3 md:w-55 md:flex-col md:items-end">
-                    <EmptySeatCount
-                        reservedSeats={schedule.reservedSeats}
-                        greenSeats={schedule.greenSeats}
-                        gcSeats={schedule.gcSeats}
-                    />
-                    <button
-                        type="button"
-                        onClick={handleSearch}
-                        className="bg-primary order-2 rounded-lg p-2 text-white md:order-3"
-                        disabled={
-                            schedule.reservedSeats === 0 &&
-                            schedule.greenSeats === 0 &&
-                            schedule.gcSeats === 0
-                        }
+                    );
+                })()}
+            </div>
+            <div className="order-3 flex w-full items-center justify-between gap-4 md:order-2 md:flex-1">
+                <div className="text-left">
+                    <div
+                        data-testid="schedule-departure-time"
+                        className="text-2xl font-black"
                     >
-                        詳細を見る
-                    </button>
+                        {dayjs(schedule.departureTime, 'HH:mm:ss').format(
+                            'HH:mm',
+                        )}
+                    </div>
+                    <div>{departureStationName}</div>
+                </div>
+                <div className="text-primary flex flex-1 items-center gap-2">
+                    <div className="border-primary-light relative w-full border-t-4 border-dotted">
+                        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-2 group-disabled:bg-gray-300">
+                            {calculateDuration(
+                                schedule.departureTime,
+                                schedule.arrivalTime,
+                            )}
+                        </span>
+                    </div>
+                    <div className="text-2xl">
+                        <FiArrowRight />
+                    </div>
+                </div>
+                <div className="text-left">
+                    <div
+                        data-testid="schedule-arrival-time"
+                        className="text-2xl font-black"
+                    >
+                        {dayjs(schedule.arrivalTime, 'HH:mm:ss').format(
+                            'HH:mm',
+                        )}
+                    </div>
+                    <div>{arrivalStationName}</div>
                 </div>
             </div>
-        </>
+            <div className="order-2 flex flex-wrap items-center gap-2 md:order-3 md:w-55 md:flex-col md:items-end">
+                <EmptySeatCount
+                    reservedSeats={schedule.reservedSeats}
+                    greenSeats={schedule.greenSeats}
+                    gcSeats={schedule.gcSeats}
+                />
+            </div>
+        </button>
     );
 }
