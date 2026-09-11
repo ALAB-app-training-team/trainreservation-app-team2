c6d61613ed8e6966ec2b503896b944ee5295e3aa
二本柳 綾香 <nihonyanagi.ayaka@wing.sysrdc.com>
2026-09-11 14:46:24 +0900

    Webのときのスクロールバーの太さは今のままで

--- frontend/src/features/schedule/components/TrainCars/TrainCars.tsx
+++ frontend/src/features/schedule/components/TrainCars/TrainCars.tsx
@@ -60,7 +60,7 @@ export function TrainCars({
             {filteredCars.length > 0 ? (
                 <>
                     <div
-                        className="flex gap-2 overflow-x-auto"
+                        className="flex gap-2 overflow-x-auto md:scrollbar-thin"
                         data-testid="train-cars"
                     >
                         {filteredCars.map((car) => (
