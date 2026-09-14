0c60695cb21ef3c9ff0308fe34d4eddaee7a9071
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-09-14 14:54:02 +0900

    高さ調整

--- frontend/src/features/schedule/components/SelectSeatsHeader.tsx
+++ frontend/src/features/schedule/components/SelectSeatsHeader.tsx
@@ -30,7 +30,7 @@ export function SelectSeatsHeader({
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col justify-start gap-2 p-4 pb-0">
+        <div className="flex flex-col justify-start gap-2 px-4 pt-2 pb-0 md:pt-4">
             {searchRequestDto !== null ? (
                 <button
                     data-testid={'back-button-in-selectseat'}
