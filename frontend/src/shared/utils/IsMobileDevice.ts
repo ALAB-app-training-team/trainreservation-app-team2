795189fbddd0acf18356a2278c6b3c324370bb01
terasaki.kaho <terasaki.kaho@wing.sysrdc.com>
2026-09-18 11:28:14 +0900

    判定式見直し

--- frontend/src/shared/utils/IsMobileDevice.ts
+++ frontend/src/shared/utils/IsMobileDevice.ts
@@ -1,9 +1,16 @@
+const MOBILE_MEDIA_QUERY = '(pointer: coarse) and (max-width: 767px)';
+
 export const isMobileDevice = () => {
-    if (typeof navigator === 'undefined') return false;
+    if (typeof window === 'undefined') return false;
 
     const userAgent = navigator.userAgent;
-    const isIPadOS =
-        userAgent.includes('Macintosh') && navigator.maxTouchPoints > 1;
 
-    return /iPhone|iPad|iPod|Android/i.test(userAgent) || isIPadOS;
+    const isDesktopModeMobile =
+        /Macintosh|X11|Linux/.test(userAgent) && navigator.maxTouchPoints > 1;
+
+    return (
+        /iPhone|iPad|iPod|Android/i.test(userAgent) ||
+        isDesktopModeMobile ||
+        window.matchMedia(MOBILE_MEDIA_QUERY).matches
+    );
 };
